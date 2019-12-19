4bcec70426adfb29279c9b8f85a67124443f93d2
Nicolas jose Dos santos <nicolas.dossants@laposte.net>
2019-12-19 09:24:43 +0100

    Routes

--- BlogAstro/src/app/app.module.ts
+++ BlogAstro/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { SuiModule } from 'ng2-semantic-ui';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConnectionComponent } from './connection/connection.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: ConnectionComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnectionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SuiModule
+    SuiModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
