96ea2213e2ff6ac238350267b8b68401ccb6c83b
Nicolas jose Dos santos <nicolas.dossants@laposte.net>
2020-01-09 17:33:57 +0100

    config droit de requêtes

--- API/server.js
+++ API/server.js
@@ -4,6 +4,13 @@ var apiRouter = require('./apiRouter.js').router;
 
 var server = express();
 
+server.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use('/AstroBlog/', apiRouter);
